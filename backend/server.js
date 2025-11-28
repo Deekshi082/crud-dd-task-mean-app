b758ef89e7c0f9c832d244a8cc10132f6dba3c0c
Deekshi08 <deekshi082@gmail.com>
2025-11-28 16:29:20 +0530

    Initial: MEAN app + Dockerfiles + docker-compose

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true); // or false if preferred
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Allow requests from your frontend
+app.use(cors({
+  origin: ['http://localhost:4200', 'http://localhost:8081', 'http://frontend:80'], // frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Test application." });
+});
+
+require("./app/routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
